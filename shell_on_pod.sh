92c48cc750cf8c89a8559d7c54ed3433f7f1eae1
David Taylor <dataylor@yogac940.tfound.org>
2022-11-21 09:36:22 -0500

    updating shell on pod.sh to use kubectl if available

--- shell_on_pod.sh
+++ shell_on_pod.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
-zenoss_pod=$(oc get pods | grep zenoss4-core | awk '{print $1}')
-oc rsh $zenoss_pod
+#!/bin/bash -x
+which oc > /dev/null 2> /dev/null
+if [[ $? -eq 0 ]]
+then
+  zenoss_pod=$(oc get pods | grep zenoss4-core | awk '{print $1}')
+  oc rsh $zenoss_pod
+  exit 0
+fi
+which kubectl > /dev/null 2> /dev/null
+if [[ $? -eq 0 ]]
+then
+  zenoss_pod=$(kubectl -n monitoring get pods | grep zenoss4-core | awk '{print $1}')
+  kubectl exec -n monitoring -i -t ${zenoss_pod} -- bash
+  exit 0
+else
+  echo -e "\nYou need a kubernetes client installed. Install openshift client 'oc' or kubectl\n\n"
+  exit 1
+fi
